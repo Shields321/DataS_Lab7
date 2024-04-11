98b2cbd12efd619f8e81caef86ec214747afced9
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-04-11 17:21:58 -0400

    Update DataS_Lab7.java

--- DataS_Lab7/src/datas_lab7/DataS_Lab7.java
+++ DataS_Lab7/src/datas_lab7/DataS_Lab7.java
@@ -1,18 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package datas_lab7;
+package DataS_Lab7;
 
 import javax.swing.*;
 
 public class DataS_Lab7 {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            GUI app = new GUI();
+            GUI app = new GUI();            
             app.setSize(800, 600);
             app.setVisible(true);
+            MyGUI gui1 = new MyGUI();
+            gui1.setVisible(true);
         });
     }
-    
 }
